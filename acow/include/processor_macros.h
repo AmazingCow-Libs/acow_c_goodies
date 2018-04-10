a103b4e91708029c54b3482b2d17bfc1083e0651
n2omatt <n2omatt@amazingcow.com>
2018-04-10 03:59:36 -0700

    - Fix macros on MSVS.

--- acow/include/processor_macros.h
+++ acow/include/processor_macros.h
@@ -25,7 +25,7 @@
 //----------------------------------------------------------------------------//
 
 // COWTODO(n2omatt): Add other processor archs...
-#if _WIN64  || __x86_64__ || __ppc64__
+#if (_WIN64)  || (__x86_64__) || (__ppc64__)
     #define ACOW_PROCESSOR_32BIT 0
     #define ACOW_PROCESSOR_64BIT 1
 #else
@@ -33,10 +33,11 @@
     #define ACOW_PROCESSOR_64BIT 0
 #endif
 
+
 //COWTODO(n2omatt): Implelement for other processors family.
 //x86 family
 //  Taken from: http://nadeausoftware.com/articles/2012/02/c_c_tip_how_detect_processor_type_using_compiler_predefined_macros
-#if i386 || __i386 || __i386__ || _M_IX86 || _X86_
+#if defined(i386) || defined(__i386) || defined(__i386__) || defined(_M_IX86) || defined(_X86_)
     #define ACOW_PROCESSOR_INTELX86  1
     #define ACOW_PROCESSOR_INTELIA64 0
     #define ACOW_PROCESSOR_ARM       0
