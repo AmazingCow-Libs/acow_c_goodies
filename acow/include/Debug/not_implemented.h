e5bd683372f32488edda11b489b36aa17254658d
n2omatt <n2omatt@amazingcow.com>
2018-04-16 13:26:10 -0700

    - Start implementing.

---
+++ acow/include/Debug/not_implemented.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "acow/include/Discovery/compiler_macros.h"
+
+// COWTODO(n2omatt): Implement....
+#define ACOW_WARNING(_arg_) do { } while(0)
+
+#define ACOW_NOT_IMPLEMENTED() \
+    ACOW_WARNING(FIXME: Code removed because...)
